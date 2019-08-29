b8e6601f2d8b16b2434e05013b84d0c5a75a67ea
Brian Orr <myemail@brianorr.me>
2019-08-28 21:55:15 -0500

    BaseApp GO!

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }               from '@angular/core';
+import { RouterModule, Routes }   from '@angular/router';
+
+// Modules Pages ---------------------------------------------------------------
+import { HomeComponent } from './modules/home/pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
